dd84c4d1efcbb49826811c40e1da968bc67e2ec2
Becky Smith <becky.smith@hogarthww.com>
2015-02-13 10:37:07 +0000

    management tests

--- booking/tests/test_management.py
+++ booking/tests/test_management.py
@@ -1,8 +1,14 @@
 from django.test import TestCase
 from django.core import management
-from allauth.socialaccount.models import SocialApp
+from django.db.models import Q
 from django.contrib.auth.models import User
+from django.utils import timezone
+from datetime import datetime, timedelta
+from allauth.socialaccount.models import SocialApp
+from mock import patch
+
 from booking.models import Event, Booking
+from booking.utils import create_classes
 
 
 class ManagementCommandsTests(TestCase):
@@ -15,16 +21,42 @@ class ManagementCommandsTests(TestCase):
     def test_load_users(self):
         self.assertEquals(User.objects.all().count(), 0)
         management.call_command('load_users')
-        self.assertTrue(User.objects.all().count() >= 5)
+        self.assertEquals(User.objects.all().count(), 5)
 
     def test_create_events(self):
         self.assertEquals(Event.objects.all().count(), 0)
         management.call_command('create_events')
-        self.assertTrue(Event.objects.all().count() >= 5)
+        self.assertEquals(Event.objects.all().count(), 5)
+
+    @patch.object(timezone, 'now',
+                  return_value=datetime(2015, 2, 10, tzinfo=timezone.utc))
+    def test_create_classes_with_manage_command(self, mock_now):
+        self.assertEquals(Event.objects.all().count(), 0)
+        management.call_command('create_classes')
+        # check that there are now classes on the Monday of the mocked week
+        # (mocked now is Wed 10 Feb 2015)
+        mon_classes = Event.objects.filter(
+            Q(date__gte=mock_now) & Q(date__lte=datetime(2015, 2, 11, tzinfo=timezone.utc))
+        )
+        self.assertTrue(mon_classes)
+        # check that there are now classes on the Monday of the following week
+        # (mocked now is Wed 10 Feb 2015)
+        next_mon_classes = Event.objects.filter(
+            Q(date__gte=datetime(2015, 2, 17, tzinfo=timezone.utc)) &
+            Q(date__lte=datetime(2015, 2, 18, tzinfo=timezone.utc))
+        )
+        self.assertTrue(next_mon_classes)
 
     def test_create_classes(self):
-        pass
-        #TODO mock now, test classes created for this week and next
+        # create classes for a given date (22/3/16 is a Tues)
+        date = datetime(2016, 3, 22, tzinfo=timezone.utc)
+        self.assertEquals(Event.objects.all().count(), 0)
+        create_classes(input_date=date)
+        # check that there are now classes on the Monday that week
+        mon_classes = Event.objects.filter(date__gte=date,
+                                           date__lte=date + timedelta(days=1))
+        self.assertTrue(mon_classes)
+
 
     def test_create_bookings(self):
         self.assertEquals(Event.objects.all().count(), 0)
--- booking/utils.py
+++ booking/utils.py
@@ -2,141 +2,147 @@ from django.utils import timezone
 from datetime import time, timedelta, datetime, date
 from booking.models import Event
 
-def create_classes(week='this'):
 
-        today = date.today()
-        if week == 'next':
-            today = date.today() + timedelta(7)
+def create_classes(week='this', input_date=None):
+    """
+    Creates a week's classes (mon-sun) from any given date.  Will create classes
+    in the past if the date given is after Monday and week is "this".
+    If no date is given, creates classes for the current week, or the next week.
+    """
+    if input_date == None:
+        input_date = date.today()
+    if week == 'next':
+        input_date = input_date + timedelta(7)
 
-        mon = today - timedelta(days=today.weekday())
-        tues = mon + timedelta(days=1)
-        wed = mon + timedelta(days=2)
-        thurs = mon + timedelta(days=3)
-        fri = mon + timedelta(days=4)
-        sat = mon + timedelta(days=5)
-        sun = mon + timedelta(days=6)
+    mon = input_date - timedelta(days=input_date.weekday())
+    tues = mon + timedelta(days=1)
+    wed = mon + timedelta(days=2)
+    thurs = mon + timedelta(days=3)
+    fri = mon + timedelta(days=4)
+    sat = mon + timedelta(days=5)
+    sun = mon + timedelta(days=6)
 
-        # Monday classes
-        Event.objects.get_or_create(
-            name="Pole Level 3",
-            type=Event.POLE_CLASS,
-            date=datetime.combine(mon, time(hour=17, minute=45, tzinfo=timezone.utc)),
-            max_participants=15,
-            cost=7,
-            payment_open=True,
-            advance_payment_required=False,
-        )
+    # Monday classes
+    Event.objects.get_or_create(
+        name="Pole Level 3",
+        type=Event.POLE_CLASS,
+        date=datetime.combine(mon, time(hour=17, minute=45, tzinfo=timezone.utc)),
+        max_participants=15,
+        cost=7,
+        payment_open=True,
+        advance_payment_required=False,
+    )
 
-        Event.objects.get_or_create(
-            name="Pole Level 1",
-            type=Event.POLE_CLASS,
-            date=datetime.combine(mon, time(hour=19, minute=0, tzinfo=timezone.utc)),
-            max_participants=15,
-            cost=7,
-            payment_open=True,
-            advance_payment_required=False,
-        )
+    Event.objects.get_or_create(
+        name="Pole Level 1",
+        type=Event.POLE_CLASS,
+        date=datetime.combine(mon, time(hour=19, minute=0, tzinfo=timezone.utc)),
+        max_participants=15,
+        cost=7,
+        payment_open=True,
+        advance_payment_required=False,
+    )
 
-        Event.objects.get_or_create(
-            name="Pole practice",
-            type=Event.OTHER_CLASS,
-            date=datetime.combine(mon, time(hour=20, minute=10, tzinfo=timezone.utc)),
-            max_participants=15,
-            cost=3.50,
-            payment_open=True,
-            advance_payment_required=False,
-        )
+    Event.objects.get_or_create(
+        name="Pole practice",
+        type=Event.OTHER_CLASS,
+        date=datetime.combine(mon, time(hour=20, minute=10, tzinfo=timezone.utc)),
+        max_participants=15,
+        cost=3.50,
+        payment_open=True,
+        advance_payment_required=False,
+    )
 
-        # Tuesday classes
-        Event.objects.get_or_create(
-            name="Pole Level 2",
-            type=Event.POLE_CLASS,
-            date=datetime.combine(tues, time(hour=17, minute=45, tzinfo=timezone.utc)),
-            max_participants=15,
-            cost=7,
-            payment_open=True,
-            advance_payment_required=False,
-        )
+    # Tuesday classes
+    Event.objects.get_or_create(
+        name="Pole Level 2",
+        type=Event.POLE_CLASS,
+        date=datetime.combine(tues, time(hour=17, minute=45, tzinfo=timezone.utc)),
+        max_participants=15,
+        cost=7,
+        payment_open=True,
+        advance_payment_required=False,
+    )
 
-        Event.objects.get_or_create(
-            name="Pole Level 4",
-            type=Event.POLE_CLASS,
-            date=datetime.combine(tues, time(hour=19, minute=0, tzinfo=timezone.utc)),
-            max_participants=15,
-            cost=7,
-            payment_open=True,
-            advance_payment_required=False,
-        )
+    Event.objects.get_or_create(
+        name="Pole Level 4",
+        type=Event.POLE_CLASS,
+        date=datetime.combine(tues, time(hour=19, minute=0, tzinfo=timezone.utc)),
+        max_participants=15,
+        cost=7,
+        payment_open=True,
+        advance_payment_required=False,
+    )
 
-        Event.objects.get_or_create(
-            name="Pole Level 1",
-            type=Event.POLE_CLASS,
-            date=datetime.combine(tues, time(hour=20, minute=10, tzinfo=timezone.utc)),
-            max_participants=15,
-            cost=7,
-            payment_open=True,
-            advance_payment_required=False,
-        )
+    Event.objects.get_or_create(
+        name="Pole Level 1",
+        type=Event.POLE_CLASS,
+        date=datetime.combine(tues, time(hour=20, minute=10, tzinfo=timezone.utc)),
+        max_participants=15,
+        cost=7,
+        payment_open=True,
+        advance_payment_required=False,
+    )
 
-        # Wednesday classes
-        Event.objects.get_or_create(
-            name="Pole conditioning",
-            type=Event.OTHER_CLASS,
-            date=datetime.combine(wed, time(hour=19, minute=0, tzinfo=timezone.utc)),
-            max_participants=15,
-            cost=3.50,
-            payment_open=True,
-            advance_payment_required=False,
-        )
+    # Wednesday classes
+    Event.objects.get_or_create(
+        name="Pole conditioning",
+        type=Event.OTHER_CLASS,
+        date=datetime.combine(wed, time(hour=19, minute=0, tzinfo=timezone.utc)),
+        max_participants=15,
+        cost=3.50,
+        payment_open=True,
+        advance_payment_required=False,
+    )
 
-        # Thursday classes
-        Event.objects.get_or_create(
-            name="Pole Mixed Levels",
-            type=Event.POLE_CLASS,
-            date=datetime.combine(thurs, time(hour=11, minute=0, tzinfo=timezone.utc)),
-            max_participants=15,
-            cost=7,
-            payment_open=True,
-            advance_payment_required=False,
-        )
+    # Thursday classes
+    Event.objects.get_or_create(
+        name="Pole Mixed Levels",
+        type=Event.POLE_CLASS,
+        date=datetime.combine(thurs, time(hour=11, minute=0, tzinfo=timezone.utc)),
+        max_participants=15,
+        cost=7,
+        payment_open=True,
+        advance_payment_required=False,
+    )
 
-        Event.objects.get_or_create(
-            name="Pole Level 2",
-            type=Event.POLE_CLASS,
-            date=datetime.combine(thurs, time(hour=20, minute=10, tzinfo=timezone.utc)),
-            max_participants=15,
-            cost=7,
-            payment_open=True,
-            advance_payment_required=False,
-        )
+    Event.objects.get_or_create(
+        name="Pole Level 2",
+        type=Event.POLE_CLASS,
+        date=datetime.combine(thurs, time(hour=20, minute=10, tzinfo=timezone.utc)),
+        max_participants=15,
+        cost=7,
+        payment_open=True,
+        advance_payment_required=False,
+    )
 
-        # Friday classes
-        Event.objects.get_or_create(
-            name="Pole Level 1",
-            type=Event.POLE_CLASS,
-            date=datetime.combine(fri, time(hour=17, minute=45, tzinfo=timezone.utc)),
-            max_participants=15,
-            cost=7,
-            payment_open=True,
-            advance_payment_required=False,
-        )
+    # Friday classes
+    Event.objects.get_or_create(
+        name="Pole Level 1",
+        type=Event.POLE_CLASS,
+        date=datetime.combine(fri, time(hour=17, minute=45, tzinfo=timezone.utc)),
+        max_participants=15,
+        cost=7,
+        payment_open=True,
+        advance_payment_required=False,
+    )
 
-        Event.objects.get_or_create(
-            name="Pole Level 3",
-            type=Event.POLE_CLASS,
-            date=datetime.combine(fri, time(hour=19, minute=0, tzinfo=timezone.utc)),
-            max_participants=15,
-            cost=7,
-            payment_open=True,
-            advance_payment_required=False,
-        )
+    Event.objects.get_or_create(
+        name="Pole Level 3",
+        type=Event.POLE_CLASS,
+        date=datetime.combine(fri, time(hour=19, minute=0, tzinfo=timezone.utc)),
+        max_participants=15,
+        cost=7,
+        payment_open=True,
+        advance_payment_required=False,
+    )
 
-        Event.objects.get_or_create(
-            name="Pole practice",
-            type=Event.OTHER_CLASS,
-            date=datetime.combine(fri, time(hour=20, minute=10, tzinfo=timezone.utc)),
-            max_participants=15,
-            cost=3.50,
-            payment_open=True,
-            advance_payment_required=False,
-        )
+    Event.objects.get_or_create(
+        name="Pole practice",
+        type=Event.OTHER_CLASS,
+        date=datetime.combine(fri, time(hour=20, minute=10, tzinfo=timezone.utc)),
+        max_participants=15,
+        cost=3.50,
+        payment_open=True,
+        advance_payment_required=False,
+    )
