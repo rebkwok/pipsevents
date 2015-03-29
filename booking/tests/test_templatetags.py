f5050a9ed96fb9b3b92399c16bfec46f8e595438
Becky Smith <rebkwok@gmail.com>
2015-03-29 11:08:37 +0100

    add cancellation period to event detail template

--- booking/tests/test_templatetags.py
+++ booking/tests/test_templatetags.py
@@ -2,7 +2,7 @@ from django.core.urlresolvers import reverse
 from django.test import TestCase, RequestFactory
 from django.test.client import Client
 from model_mommy import mommy
-from booking.views import EventDetailView
+from booking.views import EventDetailView, BookingDetailView
 from booking.tests.helpers import set_up_fb
 
 
@@ -21,7 +21,7 @@ class BookingtagTests(TestCase):
         view = EventDetailView.as_view()
         return view(request, slug=event.slug)
 
-    def test_cancellation_format_tag(self):
+    def test_cancellation_format_tag_event_detail(self):
         """
         Test that cancellation period is formatted correctly
         """
@@ -44,3 +44,17 @@ class BookingtagTests(TestCase):
         resp = self._get_response(self.user, event)
         resp.render()
         self.assertIn('1 week, 1 day and 0 hours', str(resp.content))
+
+    def test_cancellation_format_tag_booking_detail(self):
+        """
+        Test that cancellation period is formatted correctly
+        """
+        event = mommy.make_recipe('booking.future_EV', cancellation_period=24)
+        booking = mommy.make_recipe('booking.booking', event=event, user=self.user)
+        url = reverse('booking:booking_detail', args=[booking.pk])
+        request = self.factory.get(url)
+        request.user = self.user
+        view = BookingDetailView.as_view()
+        resp = view(request, pk=booking.pk)
+        resp.render()
+        self.assertIn('24 hours', str(resp.content))
