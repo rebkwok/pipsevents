2a676b895e52031965646d55975b0e3e2d3f56c6
Becky Smith <rebkwok@gmail.com>
2015-03-29 20:04:52 +0100

    success url for profile update

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, HttpResponse, get_object_or_404
 from django.views.generic import UpdateView
 from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
 from braces.views import LoginRequiredMixin
 
 
@@ -16,3 +17,7 @@ class ProfileUpdateView(LoginRequiredMixin, UpdateView):
 
     def get_object(self):
         return get_object_or_404(User, username=self.request.user.username, email=self.request.user.email)
+
+    def get_success_url(self):
+        return reverse('profile:profile')
+
