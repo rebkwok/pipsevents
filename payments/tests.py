18d28e6c04d3a90edbd737f45b56ce9cce5ac245
Becky Smith <rebkwok@gmail.com>
2015-07-05 13:13:24 -0700

    payment helper tests

--- payments/tests.py
+++ payments/tests.py
@@ -4,7 +4,8 @@ from django.test import TestCase
 from django.core.urlresolvers import reverse
 
 from booking.tests.helpers import set_up_fb
-
+from payments import helpers
+from payments.models import PaypalBookingTransaction, PaypalBlockTransaction
 
 class TestViews(TestCase):
 
@@ -58,3 +59,76 @@ class TestViews(TestCase):
         url = reverse('payments:paypal_cancel')
         resp = self.client.post(url)
         self.assertEqual(resp.status_code, 200)
+
+class TestHelpers(TestCase):
+
+    def test_create_booking_transaction(self):
+        user = mommy.make_recipe('booking.user', username="testuser")
+        booking = mommy.make_recipe(
+            'booking.booking', user=user, event__name='test event'
+        )
+        booking_txn = helpers.create_booking_paypal_transaction(user, booking)
+        self.assertEqual(booking_txn.booking, booking)
+        self.assertEqual(
+            booking_txn.invoice_id,
+            'testuser-te-{}-inv#001'.format(
+                booking.event.date.strftime("%d%m%y%H%M")
+            )
+        )
+
+    def test_create_existing_booking_transaction(self):
+        user = mommy.make_recipe('booking.user', username="testuser")
+        booking = mommy.make_recipe(
+            'booking.booking', user=user, event__name='test event'
+        )
+        booking_txn = helpers.create_booking_paypal_transaction(user, booking)
+        self.assertEqual(booking_txn.booking, booking)
+        self.assertEqual(
+            booking_txn.invoice_id,
+            'testuser-te-{}-inv#001'.format(
+                booking.event.date.strftime("%d%m%y%H%M")
+            )
+        )
+        self.assertEqual(PaypalBookingTransaction.objects.count(), 1)
+
+        dp_booking_txn = helpers.create_booking_paypal_transaction(user, booking)
+        self.assertEqual(PaypalBookingTransaction.objects.count(), 1)
+        self.assertEqual(booking_txn, dp_booking_txn)
+
+    def test_create_existing_booking_txn_with_txn_id(self):
+        """
+        if the existing transaction is already associated with a paypal
+        transaction_id, we do need to create a new transaction, with incremented
+        counter
+        """
+        user = mommy.make_recipe('booking.user', username="testuser")
+        booking = mommy.make_recipe(
+            'booking.booking', user=user, event__name='test event'
+        )
+        booking_txn = helpers.create_booking_paypal_transaction(user, booking)
+        self.assertEqual(booking_txn.booking, booking)
+        self.assertEqual(
+            booking_txn.invoice_id,
+            'testuser-te-{}-inv#001'.format(
+                booking.event.date.strftime("%d%m%y%H%M")
+            )
+        )
+        self.assertEqual(PaypalBookingTransaction.objects.count(), 1)
+        booking_txn.transaction_id = "123"
+        booking_txn.save()
+        new_booking_txn = helpers.create_booking_paypal_transaction(user, booking)
+        self.assertEqual(PaypalBookingTransaction.objects.count(), 2)
+        self.assertEqual(
+            new_booking_txn.invoice_id,
+            'testuser-te-{}-inv#002'.format(
+                booking.event.date.strftime("%d%m%y%H%M")
+            )
+        )
+    def test_do_not_create_duplicate_invoice_id(self):
+        pass
+
+    def test_create_block_transaction(self):
+        pass
+
+    def test_create_existing_block_transaction(self):
+        pass
