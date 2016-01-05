1a06bb91a116bed87f55d6ba29a3f8977d15e01c
Becky Smith <rebkwok@gmail.com>
2016-01-05 00:31:30 +0000

    Improve formatting for block in registers

--- booking/templatetags/bookingtags.py
+++ booking/templatetags/bookingtags.py
@@ -137,3 +137,12 @@ def sale_text():
 def in_group(user, group_name):
     group = Group.objects.get(name=group_name)
     return True if group in user.groups.all() else False
+
+@register.filter
+def format_block(block):
+    return "{} ({}/{} left); expires {}".format(
+            block.block_type.event_type.subtype,
+            block.block_type.size - block.bookings_made(),
+            block.block_type.size,
+            block.expiry_date.strftime('%d %b %y')
+        )
