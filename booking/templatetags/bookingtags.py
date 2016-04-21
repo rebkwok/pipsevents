d086ba0cceb19ba1404c28908b509f81c490e2cf
Becky Smith <rebkwok@gmail.com>
2016-04-21 12:54:26 +0100

    Fix some errors with block displays on register template

--- booking/templatetags/bookingtags.py
+++ booking/templatetags/bookingtags.py
@@ -159,7 +159,7 @@ def in_group(user, group_name):
 @register.filter
 def format_block(block):
     if not block:
-        return "No active block"
+        return "Active block not used"
     return "{} ({}/{} left); expires {}".format(
             block.block_type.event_type.subtype,
             block.block_type.size - block.bookings_made(),
