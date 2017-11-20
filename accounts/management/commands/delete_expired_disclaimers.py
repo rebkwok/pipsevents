5cdf3d6722c93bbd3911c86631f78abf93fd09ca
Becky Smith <rebkwok@gmail.com>
2017-11-20 23:17:02 +0000

    Delete online and print disclaimers > 3 yrs old We expire disclaimers after 1 yr and require re-signing, so we don't need to check if they've booked classes in the past year. As personal injury claims can be submitted up to 3 years afterwards, keep disclaimer info for 3 years irrespective of booking.

--- accounts/management/commands/delete_expired_disclaimers.py
+++ accounts/management/commands/delete_expired_disclaimers.py
@@ -1,9 +1,5 @@
 '''
-Check for users who do not have a paid class or event booking within the past
-year.
-Delete both Print and Online disclaimers (there should only be one or the
-other, but check for both just in case)
-Email studio notification and note whether online or paper or both
+Delete all disclaimers signed or updated > 3 yrs ago
 ActivityLog it
 '''
 import logging
@@ -17,6 +13,8 @@ from django.template.loader import get_template
 from django.core.management.base import BaseCommand
 from django.db.models import Q
 
+from dateutil.relativedelta import relativedelta
+
 from accounts.models import OnlineDisclaimer, PrintDisclaimer
 from booking.email_helpers import send_support_email
 from activitylog.models import ActivityLog
@@ -26,32 +24,17 @@ logger = logging.getLogger(__name__)
 
 
 class Command(BaseCommand):
-    help = 'Delete disclaimers for users who do not have a paid booking ' \
-           'within the past year'
+    help = "Delete online disclaimers over 3 years old and print disclaimers " \
+           "for users who haven't booked in past year"
 
     def handle(self, *args, **options):
 
         # get relevant users
 
-        expire_date = timezone.now() - timedelta(days=365)
-
-        expired_users = []
-
-        for user in User.objects.all():
-            if not user.bookings.exists():
-                expired_users.append(user)
-            else:
-                recent_bookings = [
-                    True for booking in user.bookings.all()
-                    if (booking.event.date > expire_date) and booking.paid
-                    ]
-                if not recent_bookings:
-                    expired_users.append(user)
+        expire_date = timezone.now() - relativedelta(years=3)
 
         old_print_disclaimers_to_delete = PrintDisclaimer.objects\
-            .select_related('user').filter(
-            date__lt=expire_date, user__in=expired_users
-        )
+            .select_related('user').filter(date__lt=expire_date)
         print_disclaimer_users = [
             '{} {}'.format(disc.user.first_name, disc.user.last_name)
             for disc in old_print_disclaimers_to_delete
@@ -60,8 +43,7 @@ class Command(BaseCommand):
         old_online_disclaimers_to_delete = OnlineDisclaimer.objects\
             .select_related('user').filter(
             Q(date__lt=expire_date) &
-            (Q(date_updated__isnull=True) | Q(date_updated__lt=expire_date)) &
-            Q(user__in=expired_users)
+            (Q(date_updated__isnull=True) | Q(date_updated__lt=expire_date))
         )
         online_disclaimer_users = [
             '{} {}'.format(disc.user.first_name, disc.user.last_name)
@@ -72,13 +54,15 @@ class Command(BaseCommand):
 
         if print_disclaimer_users:
             ActivityLog.objects.create(
-                log='Print disclaimers deleted for expired users: {}'.format(
+                log='Print disclaimers more than 3 yrs old deleted for '
+                    'users: {}'.format(
                     ', '.join(print_disclaimer_users)
                 )
             )
         if online_disclaimer_users:
             ActivityLog.objects.create(
-                log='Online disclaimers deleted for expired users: {}'.format(
+                log='Online disclaimers more than 3 yrs old deleted for '
+                    'users: {}'.format(
                     ', '.join(online_disclaimer_users)
                 )
             )
