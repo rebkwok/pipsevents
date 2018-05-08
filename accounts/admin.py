7b020d969807c2a21c9eec940172a8240796f530
Becky Smith <becky.smith@ecometrica.com>
2018-05-08 16:50:29 +0100

    Split out data privacy and cookie policies

--- accounts/admin.py
+++ accounts/admin.py
@@ -4,7 +4,7 @@ from django import forms
 from ckeditor.widgets import CKEditorWidget
 
 from accounts.models import OnlineDisclaimer, PrintDisclaimer, \
-    DataPrivacyPolicy, SignedDataPrivacy
+    CookiePolicy, DataPrivacyPolicy, SignedDataPrivacy
 
 
 class OnlineDisclaimerAdmin(admin.ModelAdmin):
@@ -28,18 +28,57 @@ class PrintDisclaimerAdmin(admin.ModelAdmin):
     readonly_fields = ('user', 'date')
 
 
+class CookiePolicyAdminForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['content'].widget = CKEditorWidget()
+        self.fields['version'].required = False
+        if not self.instance.id:
+            current_policy = CookiePolicy.current()
+            if current_policy:
+                self.fields['content'].initial = current_policy.data_privacy_content
+                self.fields[
+                    'version'].help_text = 'Current version is {}.  Leave ' \
+                                           'blank for next major ' \
+                                           'version'.format(current_policy.version)
+            else:
+                self.fields['version'].initial = 1.0
+
+    def clean(self):
+        new_content = self.cleaned_data.get('content')
+
+        # check content has changed
+        current_policy = CookiePolicy.current()
+        if current_policy and current_policy.content == new_content:
+            self.add_error(
+                None, 'No changes made from previous version; '
+                      'new version must update data privacy content'
+            )
+
+    def save(self, *args, **kwargs):
+        self.clean()
+        return super().save(*args, **kwargs)
+
+    class Meta:
+        model = CookiePolicy
+        fields = '__all__'
+
+
+class CookiePolicyAdmin(admin.ModelAdmin):
+    readonly_fields = ('issue_date',)
+    form = CookiePolicyAdminForm
+
+
 class DataPrivacyPolicyAdminForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['data_privacy_content'].widget = CKEditorWidget()
-        self.fields['cookie_content'].widget = CKEditorWidget()
+        self.fields['content'].widget = CKEditorWidget()
         self.fields['version'].required = False
         if not self.instance.id:
             current_dp = DataPrivacyPolicy.current()
             if current_dp:
-                self.fields['data_privacy_content'].initial = current_dp.data_privacy_content
-                self.fields['cookie_content'].initial = current_dp.cookie_content
+                self.fields['content'].initial = current_dp.data_privacy_content
                 self.fields['version'].help_text = 'Current version is {}.  Leave ' \
                                                    'blank for next major ' \
                                                    'version'.format(current_dp.version)
@@ -47,22 +86,19 @@ class DataPrivacyPolicyAdminForm(forms.ModelForm):
                 self.fields['version'].initial = 1.0
 
     def clean(self):
-        new_privacy_content = self.cleaned_data.get('data_privacy_content')
-        new_cookie_content = self.cleaned_data.get('cookie_content')
+        new_content = self.cleaned_data.get('content')
 
         # check content has changed
         current = DataPrivacyPolicy.current()
-        if current and current.data_privacy_content == new_privacy_content \
-            and current.cookie_content == new_cookie_content:
+        if current and current.content == new_content:
             self.add_error(
                 None, 'No changes made from previous version; '
-                      'new version must update data privacy or cookie policy '
-                      'content'
+                      'new version must update data privacy content'
             )
             
     def save(self, *args, **kwargs):
         self.clean()
-        return super(DataPrivacyPolicyAdminForm, self).save(*args, **kwargs)
+        return super().save(*args, **kwargs)
 
     class Meta:
         model = DataPrivacyPolicy
@@ -81,4 +117,5 @@ class SignedDataPrivacyAdmin(admin.ModelAdmin):
 admin.site.register(OnlineDisclaimer, OnlineDisclaimerAdmin)
 admin.site.register(PrintDisclaimer, PrintDisclaimerAdmin)
 admin.site.register(DataPrivacyPolicy, DataPrivacyPolicyAdmin)
+admin.site.register(CookiePolicy, CookiePolicyAdmin)
 admin.site.register(SignedDataPrivacy, SignedDataPrivacyAdmin)
--- accounts/tests/test_models.py
+++ accounts/tests/test_models.py
@@ -9,7 +9,7 @@ from django.core.exceptions import ValidationError
 from django.test import TestCase, override_settings
 from django.utils import timezone
 
-from accounts.models import DataPrivacyPolicy, SignedDataPrivacy, \
+from accounts.models import CookiePolicy, DataPrivacyPolicy, SignedDataPrivacy, \
     PrintDisclaimer, OnlineDisclaimer, \
     DISCLAIMER_TERMS, MEDICAL_TREATMENT_TERMS, OVER_18_TERMS
 from accounts.utils import has_active_data_privacy_agreement, \
@@ -86,42 +86,57 @@ class DataPrivacyPolicyModelTests(TestCase):
 
     def test_policy_versioning(self):
         self.assertEqual(DataPrivacyPolicy.current_version(), 0)
-        DataPrivacyPolicy.objects.create(
-            data_privacy_content='Foo', cookie_content='Bar'
-        )
 
+        DataPrivacyPolicy.objects.create(content='Foo')
         self.assertEqual(DataPrivacyPolicy.current_version(), Decimal('1.0'))
-        DataPrivacyPolicy.objects.create(
-            data_privacy_content='Foo', cookie_content='Bar1'
-        )
+
+        DataPrivacyPolicy.objects.create(content='Foo1')
         self.assertEqual(DataPrivacyPolicy.current_version(), Decimal('2.0'))
 
-        DataPrivacyPolicy.objects.create(
-            data_privacy_content='Foo', cookie_content='Bar2', version=Decimal('2.6')
-        )
+        DataPrivacyPolicy.objects.create(content='Foo2', version=Decimal('2.6'))
         self.assertEqual(DataPrivacyPolicy.current_version(), Decimal('2.6'))
 
-        DataPrivacyPolicy.objects.create(
-            data_privacy_content='Foo', cookie_content='Bar3'
-        )
+        DataPrivacyPolicy.objects.create(content='Foo3')
         self.assertEqual(DataPrivacyPolicy.current_version(), Decimal('3.0'))
 
     def test_cannot_make_new_version_with_same_content(self):
-        DataPrivacyPolicy.objects.create(
-            data_privacy_content='Foo', cookie_content='Bar'
-        )
+        DataPrivacyPolicy.objects.create(content='Foo')
         self.assertEqual(DataPrivacyPolicy.current_version(), Decimal('1.0'))
         with self.assertRaises(ValidationError):
-            DataPrivacyPolicy.objects.create(
-                data_privacy_content='Foo', cookie_content='Bar'
-            )
+            DataPrivacyPolicy.objects.create(content='Foo')
 
     def test_policy_str(self):
-        dp = DataPrivacyPolicy.objects.create(
-            data_privacy_content='Foo', cookie_content='Bar'
-        )
+        dp = DataPrivacyPolicy.objects.create(content='Foo')
         self.assertEqual(
-            str(dp), 'Version {}'.format(dp.version)
+            str(dp), 'Data Privacy Policy - Version {}'.format(dp.version)
+        )
+
+
+class CookiePolicyModelTests(TestCase):
+
+    def test_policy_versioning(self):
+        CookiePolicy.objects.create(content='Foo')
+        self.assertEqual(CookiePolicy.current().version, Decimal('1.0'))
+
+        CookiePolicy.objects.create(content='Foo1')
+        self.assertEqual(CookiePolicy.current().version, Decimal('2.0'))
+
+        CookiePolicy.objects.create(content='Foo2', version=Decimal('2.6'))
+        self.assertEqual(CookiePolicy.current().version, Decimal('2.6'))
+
+        CookiePolicy.objects.create(content='Foo3')
+        self.assertEqual(CookiePolicy.current().version, Decimal('3.0'))
+
+    def test_cannot_make_new_version_with_same_content(self):
+        CookiePolicy.objects.create(content='Foo')
+        self.assertEqual(CookiePolicy.current().version, Decimal('1.0'))
+        with self.assertRaises(ValidationError):
+            CookiePolicy.objects.create(content='Foo')
+
+    def test_policy_str(self):
+        dp = CookiePolicy.objects.create(content='Foo')
+        self.assertEqual(
+            str(dp), 'Cookie Policy - Version {}'.format(dp.version)
         )
 
 
@@ -129,9 +144,7 @@ class SignedDataPrivacyModelTests(TestCase):
 
     @classmethod
     def setUpTestData(cls):
-        DataPrivacyPolicy.objects.create(
-            data_privacy_content='Foo', cookie_content='Bar'
-        )
+        DataPrivacyPolicy.objects.create(content='Foo')
 
     def setUp(self):
         self.user = mommy.make_recipe('booking.user')
@@ -141,9 +154,7 @@ class SignedDataPrivacyModelTests(TestCase):
         self.assertTrue(cache.get(active_data_privacy_cache_key(self.user)))
 
         cache.clear()
-        DataPrivacyPolicy.objects.create(
-            data_privacy_content='New Foo', cookie_content='Bar'
-        )
+        DataPrivacyPolicy.objects.create(content='New Foo')
         self.assertFalse(has_active_data_privacy_agreement(self.user))
 
     def test_delete(self):
@@ -152,5 +163,3 @@ class SignedDataPrivacyModelTests(TestCase):
 
         SignedDataPrivacy.objects.get(user=self.user).delete()
         self.assertIsNone(cache.get(active_data_privacy_cache_key(self.user)))
-
-
