a6bde5d679ccf6022e714db5d152d999a5e6de10
Becky Smith <becky@brainnwave.com>
2019-03-26 15:31:43 +0000

    Sort user drop down in registers (add booking) form

--- studioadmin/forms/register_forms.py
+++ studioadmin/forms/register_forms.py
@@ -297,7 +297,7 @@ def get_user_choices(event):
 
     def callable():
         booked_user_ids = event.bookings.filter(status='OPEN', no_show=False).values_list('user_id', flat=True)
-        users = User.objects.exclude(id__in=booked_user_ids)
+        users = User.objects.exclude(id__in=booked_user_ids).order_by('first_name')
         return tuple([(user.id, "{} {} ({})".format(user.first_name, user.last_name, user.username)) for user in users])
 
     return callable
