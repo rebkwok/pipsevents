5b89dc79d0ba5fdf83a58566b8ac76ba67563c52
Becky Smith <becky@brainnwave.com>
2019-08-16 17:02:34 +0100

    Fix typo in disclaimer terms

--- accounts/models.py
+++ accounts/models.py
@@ -64,7 +64,7 @@ DISCLAIMER_TERMS = "I recognise that I may be asked to participate in some " \
                    "I am in good physical condition can participate in the " \
                    "courses offered by The Watermelon Studio. I will not " \
                    "participate if pregnant or if I have given birth within " \
-                   "the previous 12 weeks 6 weeks or C-section in 12 weeks " \
+                   "the previous 6 weeks or C-section within the previous 12 weeks " \
                    "and I will update my teacher on " \
                    "any new medical condition/injury throughout my time at " \
                    "The Watermelon Studio.  I will not participate under the " \
