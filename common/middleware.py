0a35b80ba449bdc6fd64849be5e7cbf3f43b053e
Becky Smith <rebkwok@gmail.com>
2019-10-20 14:07:01 +0100

    python3.7 compatibility

--- common/middleware.py
+++
@@ -1,8 +0,0 @@
-from django.utils.deprecation import MiddlewareMixin
-
-from responsive.middleware import DeviceInfoMiddleware
-
-
-class NewDeviceInfoMiddleware(MiddlewareMixin, DeviceInfoMiddleware):
-    """Add MiddlewareMixin for new-style django middleware"""
-    pass
