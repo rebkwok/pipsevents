7f81de1ba2acca451583181b66363641d9bd12ff
Becky Smith <rebkwok@gmail.com>
2022-09-23 13:00:02 +0100

    fix csrf view

--- common/views.py
+++ common/views.py
@@ -3,6 +3,6 @@ from django.template import loader
 
 
 def csrf_failure(request, reason=""):
-    context = {"back_url": request.META['HTTP_REFERER']}
+    context = {"back_url": request.META.get('HTTP_REFERER', "/")}
     template = loader.get_template("403_csrf.html")
     return HttpResponseForbidden(template.render(context))
