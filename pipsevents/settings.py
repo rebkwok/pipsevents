19ea9fc75707bff5303d781c9f5be708d6f78f22
Becky Smith <rebkwok@gmail.com>
2023-09-14 20:07:31 +0100

    Use memcached for caching

--- pipsevents/settings.py
+++ pipsevents/settings.py
@@ -120,8 +120,9 @@ if TESTING or env('LOCAL'):  # use local cache for tests
 else:
     CACHES = {
         'default': {
-            'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
-            'LOCATION': 'pips',
+            'BACKEND': 'django.core.cache.backends.memcached.PyMemcacheCache',
+            'LOCATION': '127.0.0.1:11211',
+            'KEY_PREFIX': 'pips',
         }
     }
 
