8eb4abf6d41146a1588b33d16d870a9f9cd3deb3
Becky Smith <rebkwok@gmail.com>
2024-05-19 09:26:57 +0100

    Store stripe customer id on user profile

--- accounts/models.py
+++ accounts/models.py
@@ -462,6 +462,7 @@ class AccountBan(models.Model):
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     pronouns = models.CharField(max_length=100, blank=True, null=True)
+    stripe_customer_id = models.CharField(max_length=50, blank=True, null=True)
 
     def __str__(self):
         return self.user.username
